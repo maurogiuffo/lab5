023b7c52dde336ea4a438e0bd3b2e07fcdae695e
mauro <maurogiuffo@gmail.com>
2019-04-29 08:56:15 -0300

    * Correccion JDBC classes import

--- thread/src/main/java/utnlab5/Database/JDBC.java
+++ thread/src/main/java/utnlab5/Database/JDBC.java
@@ -2,7 +2,14 @@ package Database;
 
 
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 
 public class JDBC {
 
